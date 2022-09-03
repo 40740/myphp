26c8bf220407e5701b6431ea7ca4e7afde2d02c6
老耗 <39937268+40740@users.noreply.github.com>
2022-09-04 07:21:18 +0800

    Create my.php

---
+++ my.php
@@ -0,0 +1,59 @@
+<?Php 
+        $loginUrl ="https://raw.nuaa.cf/cxfksword/iptv/master/daily_multi.m3u8";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$loginUrl);
+        $result=curl_exec($ch);
+        curl_close($ch); //关闭访问,释放资源 
+        
+        $v = explode('#EXTINF',$result); //拆 $ 符号为数组   
+        
+        unset($v[0]); //删掉第一项
+        
+        $data = [];
+        
+        foreach ($v as $value) {
+            
+            $t = explode("\n",$value); //把字符串以回车符号 分为数组
+            
+            //  //获取 台名称
+             $isMatched = preg_match('/(?<= tvg-name=")(.+?)(?=" tvg-logo=)/', $t[0], $matches);  
+             
+             //台名称 
+             
+            //  $matches[0];
+            
+            // 
+            
+                 $json = 
+                      [
+                        "uniacid" =>3, 
+                        "name"=> $matches[0],
+                        "sort"=> "0", //分类 0 为电视台页 1 为连续剧  2  为手机视频
+                      "pic"=> "0",
+                      "playIndex"=>"0", 
+                      "isCache"=> "0",
+                      "playtype"=> "1",
+                      "progress" =>"0",
+                      "chaoshi"=>"5000",
+                      "ScreenScaleType"=> 0,
+                      "piantou"=> "0",
+                      "pianwei"=> 0,
+                      "sourceKey"=>"push_agent",
+                      "model"=> "tv",
+                      "vod_play_from"=>"直连",
+                      "phoneshow" =>0,
+                      "vod_play_url"=>$t[1]
+                      ];  
+            
+             array_push($data,$json);
+          
+        }
+        
+        
+        
+        file_put_contents("README.MD", $data); 
+        
+     
